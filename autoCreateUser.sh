57845105b71f57eb4ab3c85bb6614de55622ecfb
qiushangwenyue <2506597416@qq.com>
2018-07-18 20:09:04 +0800

    使用函数式编程

--- autoCreateUser.sh
+++ autoCreateUser.sh
@@ -1,34 +1,46 @@
 #! /bin/bash
-echo "start to create user 'bigdata'!"
 
 #创建bigdata用户组，创建bigdata用户并设置密码
-user=bigdata
-group=bigdata1
+function createUser()
+{
+ echo "start to create user 'bigdata'!"
 
-#create group if not exists 
-#在/etc/group中查找用户组是否存在，并把错误输出输到/dev/null中
-egrep "^$group" /etc/group >& /dev/null
-# 判断上一命令是否等于0，不等于则创建用户组
-if [ $? -ne 0 ] 
-then
-    groupadd $group
-fi
+ user=$1
+ group=$2
 
-#create user if not exists
-egrep "^$user" /etc/passwd >& /dev/null
-if [ $? -ne 0 ]
-then 
-    useradd -g $group $user
-fi
+ #create group if not exists
+ #在/etc/group中查找用户组是否存在，并把错误输出输到/dev/null中
+ egrep "^$group" /etc/group >& /dev/null
+ # 判断上一命令是否等于0，不等于则创建用户组
+ if [ $? -ne 0 ]
+ then
+     groupadd $group
+ fi
 
-# passwd bigdata
+ #create user if not exists
+ egrep "^$user" /etc/passwd >& /dev/null
+ if [ $? -ne 0 ]
+ then
+     useradd -g $group $user
+ fi
+
+ passwd bigdata
+}
 
 #删除bigdata用户，删除bigdata用户组
-userdel -r $user
-if [ $user != $group ]
-then
-   groupdel $group
-fi
+function deleteUserAndGroup()
+{
+ user=$1
+ group=$2
 
+ echo "delete the user:" $user " and the userGroup:" $group
+ userdel -r $user
+ if [ $user != $group ]
+ then
+     groupdel $group
+ fi
+}
 
+#createUser bigdata bigdata
 
+#deleteUserAndGroup bigdata bigdata
