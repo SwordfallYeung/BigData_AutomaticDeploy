f98ecef1d9f5761dbba8ff432cfdd26803e3d127
swordfall <2506597416@qq.com>
2018-07-23 00:58:58 +0800

    自动化ssh-keygen -t rsa

--- sshFreePasswordToLogin.sh
+++ sshFreePasswordToLogin.sh
@@ -64,9 +64,23 @@ function sshFreeLogin()
 
 function autoCreateSSH()
 {
- #1.密钥对不存在则创建密钥
+ #1.使用yum安装expect
+ #yum -y install expect
+ #2.密钥对不存在则创建密钥
  [ ! -f /root/.ssh/id_rsa.pub ] && ssh-keygen -t rsa -P '' >& /dev/null
- 
+# while read line;do
+#        ip=`echo $line | cut -d " " -f1`         #提取文件中的ip
+#        user_name=`echo $line | cut -d " " -f2`  #提取文件中的用户名
+#        pass_word=`echo $line | cut -d " " -f3`  #提取文件中的密码
+# expect <<EOF
+#          spawn ssh-copy-id -i /root/.ssh/id_rsa.pub $user_name@$ip #复制公钥到目标主机
+#          expect {
+#                  "yes/no" { send "yes\n";exp_continue } #expect实现自动输入密码
+#                  "password" { send "$pass_word\n" }
+#          }
+#          expect eof
+#EOF
+# done < /home/hadoop/automaticDeploy/host_ip.txt  # 读取存储ip的文件
 }
 
 autoCreateSSH
