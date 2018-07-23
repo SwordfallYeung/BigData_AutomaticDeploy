bf9b3c5b995c092bb79c4f9496481e42328e978e
swordfall <2506597416@qq.com>
2018-07-24 01:10:43 +0800

    添加参数，允许运行shell文件时传入参数

--- changeHostname.sh
+++ changeHostname.sh
@@ -14,4 +14,12 @@ function changeHostname()
  echo "change the hostname successfully"
 }
 
-changeHostname node1
+#获取参数
+node=$1
+echo $node
+if [ -z $node ]
+then
+    echo "参数为空，请先输入参数"
+else
+    changeHostname $node
+fi
