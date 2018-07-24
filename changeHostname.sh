c910876866276d56e9e00b4ef204eccb37f35c0a
swordfall <2506597416@qq.com>
2018-07-24 20:22:41 +0800

    添加注释

--- changeHostname.sh
+++ changeHostname.sh
@@ -3,23 +3,27 @@
 #修改机器名hostname
 function changeHostname()
 {
- echo "change the hostname"
  hostname=$1
+ #echo "change the hostname $1"
+ 
  egrep "^HOSTNAME=" /etc/sysconfig/network >& /dev/null
  if [ $? -eq 0 ]
  then
+      #存在则删除旧的hostname
       sed -i "/^HOSTNAME=/d" /etc/sysconfig/network
  fi
+ #添加新的hostname
  echo "HOSTNAME=$hostname" >> /etc/sysconfig/network
- echo "change the hostname successfully"
+ 
+ #echo "change the hostname $1 successfully"
 }
 
 #获取参数
 node=$1
-echo $node
+
 if [ -z $node ]
 then
-    echo "参数为空，请先输入参数"
+    echo "参数为空，请输入参数node1,node2,node3..."
 else
     changeHostname $node
 fi
