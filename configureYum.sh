791fcacd7dafb4acf388b98cc309d9fa472ebd32
swordfall <2506597416@qq.com>
2018-07-25 19:05:47 +0800

    优化逻辑，增加moont是否挂载、httpd服务是否启动、yum源日志隐藏、

--- configureYum.sh
+++ configureYum.sh
@@ -4,7 +4,7 @@
 function configureYumSource()
 {
  yumUrl=$1 
- $yumFile=$2
+ yumFile=$2
  
  #1.把CentOS-Media.repo文件非#开头的行注释掉
  sed -i 's/^[^#]/#&/' $yumFile
@@ -16,9 +16,9 @@ function configureYumSource()
  echo "enabled=1" >> $yumFile
  echo "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7" >> $yumFile
  #3.清除YUM缓存
- yum clean all
+ yum clean all > /dev/null 2>&1
  #4.列出可用的YUM源
- yum repolist
+ yum repolist > /dev/null 2>&1
 }
 
 #处理并保证只有一个CentOS-Media.repo文件存在
@@ -32,7 +32,7 @@ function doWithRepoFile()
  for file in $fileResult
  do
    onlyFile=`find /etc/yum.repos.d -wholename $file.bak`
-   if [ -z $onlyFile ]
+   if [[ -z $onlyFile ]]
    then
         mv $file $file.bak
    fi
@@ -40,7 +40,7 @@ function doWithRepoFile()
 
  #3.只创建一个.repo文件用于配置本地yum源
  result=`find /etc/yum.repos.d/ -name CentOS-Media.repo.bak`
- if [ -z $result ]
+ if [[ -z $result ]]
  then
      touch CentOS-Media.repo
  else
@@ -48,11 +48,15 @@ function doWithRepoFile()
  fi
 }
 
+#############################################################################
+#同一函数及其调用的子函数，父函数与子函数均有一样的变量名，而内容不一样会报错
+#############################################################################
+
 #配置本地源
 function localYumSource()
 {
  systemUrl=$1
- yumUrl=$2
+ ip=$2
  yumFile=/etc/yum.repos.d/CentOS-Media.repo
 
  #1.不存在则创建mount的目录
@@ -60,25 +64,34 @@ function localYumSource()
  then
       mkdir /var/iso
  fi
- #挂载系统
- mount -o loop $systemUrl /var/iso
+ 
+ #挂载系统，已挂载则不再次挂载
+ if [ ! -d /var/iso/CentOS_BuildTag ]
+ then 
+     mount -o loop $systemUrl /var/iso
+ fi
 
  #2.处理并保证只有一个CentOS-Media.repo的文件用于配置本地yum源
  doWithRepoFile 
 
  #3.配置yum源
- configureYumSource file:///var/iso/ $yumFile
+ configureYumSource file:///var/iso $yumFile
 
  #4.安装相应的软件
  httpdIsExists=`rpm -qa | grep http`
- if [ -z $httpdIsExists ]
+ if [[ -z $httpdIsExists ]]
  then
      yum install -y httpd
  fi
 
  #5.开启httpd使用浏览器访问
  #service httpd start
- systemctl start httpd.service
+ httpdStatus=`systemctl status httpd.service`
+ result=$(echo $httpdStatus | grep "Active: active (running)")
+ if [[ $result = "" ]]
+ then
+     systemctl start httpd.service
+ fi
  
  #6.将YUM源配置到httpd中，其他的服务器可通过网络访问这个内网中的YUM源
  httpUrl=/var/www/html/CentOS-7.0
@@ -92,14 +105,14 @@ function localYumSource()
  umount -fl /var/iso
  
  #8.修改yum源指向的地址
- sed -i 's/^baseurl\=file:\/\/\/var\/iso/baseurl\=http:\/\/'$yumUrl'\/CentOS\-7.0/' $yumFile
+ sed -i 's/^baseurl\=file:\/\/\/var\/iso/baseurl\=http:\/\/'$ip'\/CentOS-7.0/' $yumFile
  
  #9.清除YUM缓存
- yum clean all
+ yum clean all > /dev/null 2>&1
  
  #10.列出可用的YUM源
- yum repolist
- echo "create the local yum source successfully"
+ yum repolist > /dev/null 2>&1
+ #echo "create the local yum source successfully"
 }
 
 #配置远程yum源
@@ -107,12 +120,13 @@ function remoteYumSource()
 {
  ip=$1
  yumUrl=http://$ip/CentOS-7.0
- 
+ yumFile=/etc/yum.repos.d/CentOS-Media.repo 
+
  #1.处理并保证只有一个CentOS-Media.repo的文件用于配置yum源
  doWithRepoFile
 
  #2.配置yum源
- configureYumSource $yumUrl
+ configureYumSource $yumUrl $yumFile
 }
 
 hostname=$1
