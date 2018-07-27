7f1598b6d1b6c9f2c22c659a9d90cc25ce89a887
swordfall <2506597416@qq.com>
2018-07-27 15:19:59 +0800

    添加所需软件安装拷贝到远程主机上

--- systems/clusterOperate.sh
+++ systems/clusterOperate.sh
@@ -12,7 +12,7 @@ function clusterOperate()
     if [[ $hostname = "node1" ]]
     then
         #2.本地主机操作
-        /home/hadoop/automaticDeploy/batchOperate.sh $hostname
+        /home/hadoop/automaticDeploy/systems/batchOperate.sh $hostname
     else
         #3.远程主机操作
         if ssh -n $hostname test -e /home/hadoop/automaticDeploy
@@ -20,10 +20,34 @@ function clusterOperate()
              #3.1 存在则先删除旧的
              ssh -n $hostname "rm -rf /home/hadoop/automaticDeploy"
         fi
+ 
         #3.2 把本地的automaticDeploy里面的脚本文件复制到远程主机上
         scp -r /home/hadoop/automaticDeploy/ $hostname:/home/hadoop/automaticDeploy
+
+        #3.3 把本地的/opt/frames里的软件安装包复制到远程主机的/opt/frames上
+        #判断远程主机上/opt/frames是否存在，不存在则创建 
+        if ssh -n $hostname test -e /opt/frames/;then
+            echo "存在" > /dev/null
+        else
+            ssh -n $hostname "mkdir /opt/frames"
+        fi
+   
+        #遍历需要安装的软件
+        while read lineString;
+        do
+          software=`echo $lineString | cut -d " " -f1`
+          isInstall=`echo $lineString | cut -d " " -f2`
+          if [[ $isInstall = "true" ]];then
+              if ssh -n $hostname test -e /opt/frames/$software;then
+                  echo "存在" > /dev/null
+              else  
+                  scp /opt/frames/$software $hostname:/opt/frames/$software
+              fi
+          fi
+        done < /home/hadoop/automaticDeploy/frames.txt
+ 
         #4.远程执行文件
-        ssh -n $hostname /home/hadoop/automaticDeploy/batchOperate.sh $hostname
+        ssh -n $hostname /home/hadoop/automaticDeploy/systems/batchOperate.sh $hostname
     fi
   done < /home/hadoop/automaticDeploy/host_ip.txt
 
